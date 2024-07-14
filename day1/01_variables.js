63ebbce7c0e98566d22440b2ef036058e605f61c
Vatsal Agarwal <168188372+vatsal80085@users.noreply.github.com>
2024-07-14 19:03:37 +0000

    day1 completed

---
+++ day1/01_variables.js
@@ -0,0 +1,14 @@
+console.log("VATSAL")
+const accountId = 1234232
+let accountEmail = "vatsalagarwal0808@gmail.com"
+var accountPassword = "********" //prefer not to use var for variable declaration {scope issue ho jata hai}
+accountCity = "Tokyo" //this type of variable memory allotment is also possible in javascript but is not recommended
+// not allowed // accountId = 9897918
+accountEmail = "baka@baka.com"
+accountPassword = "@@@@@@@"
+accountCity = "NYC"
+let accountState;
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
---
+++ day1/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all Js code as Newer version
+
+/* alert("Moshi moshi")
+ alert( 2 + 98) //we are using nodejs, not browser so these will generate an error
+*/
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
