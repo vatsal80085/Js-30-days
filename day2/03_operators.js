c7810ad0b63c36e206bf85dc8baec2c8a1071ff3
Vatsal Agarwal <168188372+vatsal80085@users.noreply.github.com>
2024-07-15 18:50:34 +0000

    day 2 completed

---
+++ day2/03_operators.js
@@ -0,0 +1,29 @@
+console.table(["addition= ",2+2,"subtraction=", 34-21,"Multiplication= ", 1002*2,"Quotient= ", 10101/3,"Remainder= ", 10220%7])
+let sum=19
+let num = 81
+console.log(num, sum)
+num+=sum
+console.log(num, sum)
+num-=sum
+console.log(num,sum)
+if (num<sum) {
+    console.log("orewa Monkey D Luffy")
+
+} else {
+    console.log("kaizokoni naro okutoda")
+}
+
+
+// === is the strict equal operator. It only returns a Boolean True if both the operands are equal and of the same type. If a is 2, and b is 4,
+
+let a = 2 //(True)
+let b = 4
+a===2
+b === 4 //(True)
+a === '2' //(False)
+
+//vs True for all of the following,
+
+a == 2 
+a == "2"
+2 == '2' 
