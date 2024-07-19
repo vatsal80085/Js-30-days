ed983b88199a08080de035070b1eee0826abf386
Vatsal Agarwal <168188372+vatsal80085@users.noreply.github.com>
2024-07-19 20:52:34 +0000

    day 3 completed

--- day2/03_operators.js
+++ day2/03_operators.js
@@ -16,14 +16,37 @@ if (num<sum) {
 
 // === is the strict equal operator. It only returns a Boolean True if both the operands are equal and of the same type. If a is 2, and b is 4,
 
-let a = 2 //(True)
-let b = 4
-a===2
-b === 4 //(True)
-a === '2' //(False)
+// a === 2 //(True)
+// b === 4 //(True)
+// a === '2' //(False)
 
 //vs True for all of the following,
 
-a == 2 
-a == "2"
-2 == '2' 
+// a == 2 
+// a == "2"
+// 2 == '2' 
+let a = 22
+let boy = 22
+let girl = 22
+if(boy>=22 && girl>=22){
+    console.log("you are legally allowed to marry each other")
+}
+else if(boy < 22 || girl < 22){
+    console.log("FBI! open up")
+}
+
+//ternary operator --> short hand if else
+const age = 26;
+const beverage = age >= 21 ? "Saake" : "Juice";
+console.log(beverage); 
+
+let number = -23
+let answer = number>0?"positive": "negative"
+console.log(answer);
+
+// if(a>=18){
+//     console.log("Eligible to drive")
+// }
+// else {
+//     console.log("Hold your horses kid")
+// }
---
+++ day3/04_control_structures.js
@@ -0,0 +1,44 @@
+//switch case in js
+
+const foo = 0;
+switch (foo) {
+  case -1:
+    console.log("negative 1");
+    break;
+  case 0: // Value of foo matches this criteria; execution starts from here
+    console.log(0);
+  // Forgotten break! Execution falls through
+  case 1: // no break statement in 'case 0:' so this case will run as well
+    console.log(1);
+    break; // Break encountered; will not continue into 'case 2:'
+  case 2:
+    console.log(2);
+    break;
+  default:
+    console.log("default");
+}
+// Logs 0 and 1
+let number = 63;
+let message;
+
+switch (true) {
+    case (number < 33):
+        message = "Fail";
+        break;
+    case (number >= 33 && number < 50):
+        message = "Not exactly failed but really bro";
+        break;
+    case (number > 50 && number < 75):
+        message = "still can't go home, can you?";
+        break;
+    case (number > 75 && number < 90):
+        message = "Average";
+        break;
+    case (number > 90):
+        message = "tum bhi yhi ho hum bhi yahi hain";
+        break;
+    default:
+        message = "Invalid number.";
+}
+
+console.log(message);
